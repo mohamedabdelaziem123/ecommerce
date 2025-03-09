b19237712089ba5a5ae163ad930986743708385f
mohamedabdelaziem123 <151367867+mohamedabdelaziem123@users.noreply.github.com>
2025-03-09 17:45:40 +0200

    final commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'shippingaddress/:cartid',
+    renderMode:RenderMode.Server
+  },
+  {
+    path: 'productDetails/:_id',
+    renderMode:RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
